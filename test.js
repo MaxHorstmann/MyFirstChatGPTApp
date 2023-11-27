b9bfee570421f64a1c83be428f3b9b2314dc9fc7
Max Horstmann <max.horstmann@gmail.com>
2023-11-27 09:05:07 -0500

    it works

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 console.log("starting...")
 
-const OPENAI_API_KEY = "YOUR KEY"
+const OPENAI_API_KEY = "APIKEY"
 
 let mediaRecorder;
 let recordedBlobs;
@@ -54,9 +54,81 @@ function requestTranscript() {
     fetch(url, fetchOptions)
     .then((response) => response.json())
     .then((data) => {
-        const elem = document.getElementById("result");
-        elem.innerHTML = data.text;
+        const elem = document.getElementById("convo");
+        const newDiv = document.createElement("div");
+        newDiv.innerHTML = "You: " + data.text;
+        elem.appendChild(newDiv);
+        getChatResponse(data.text);
       });
+}
+
+function getChatResponse(prompt) {
+    const url = new URL("https://api.openai.com/v1/chat/completions");
+    var body = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+          {
+            "role": "system",
+            "content": "You are Elsa from Frozen. I'm asking you questions."
+          },
+          {
+            "role": "user",
+            "content": prompt
+          }
+        ]
+      };
+    
+
+    const fetchOptions = {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+            "Authorization": "Bearer " + OPENAI_API_KEY,
+            "Content-Type": "application/json"
+        }
+      };      
+    fetch(url, fetchOptions)
+    .then((response) => response.json())
+    .then((data) => {
+        const elem = document.getElementById("convo");
+        const newDiv = document.createElement("div");
+        const response = data.choices[0].message.content
+        newDiv.innerHTML = "AI: " + response;
+        elem.appendChild(newDiv);
+        createSpeech(response);
+      });
+
+}
+
+function createSpeech(response) {
+    const url = new URL("https://api.openai.com/v1/audio/speech");
+    var body = {
+        "model": "tts-1",
+        "input": response,
+        "voice": "nova",
+        "response_format": "mp3"
+      };
+    const fetchOptions = {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+            "Authorization": "Bearer " + OPENAI_API_KEY,
+            "Content-Type": "application/json"
+        }
+      };      
+    fetch(url, fetchOptions)
+    .then((response) => {
+        response.blob()
+        .then((blob) => {
+            const blobUrl = URL.createObjectURL(blob);
+            window.audio = new Audio();
+            window.audio.src = blobUrl;
+            window.audio.controls = true;
+            document.body.appendChild(window.audio);
+            window.audio.play();
+        });
+      });
+
 }
 
 
